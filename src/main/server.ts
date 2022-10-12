4ec0505a3f7073753ac4d45f718257d0d0096f0a
rzjprogramador <rzjprogramador@gmail.com>
2022-10-12 19:42:21 -0300

    chore: scripts nodemon dev start

--- src/main/server.ts
+++ src/main/server.ts
@@ -6,4 +6,4 @@ import { env } from "@src/main/env/enviroments"
 
 const PORT: number = env.portExpress
 
-app.listen(PORT, () => console.log(`SERVER ON ${PORT}`))
+app.listen(PORT, () => console.log(`SERVER_ON >> ${PORT}`))
